1205422a064af0d39fba0ef35c8763e4560b36ab
burkay <burkaygul96@gmail.com>
2024-11-17 20:05:44 +0100

    day01_Constructors

--- src/day01_Constructors/C06_StaticVariables.java
+++ src/day01_Constructors/C06_StaticVariables.java
@@ -11,14 +11,12 @@ public class C06_StaticVariables {
         obj1.numberStatic++;
         obj1.numberInstance++;
 
-        System.out.println("obj1 numberStatic: "+obj1.numberStatic+ " obj1 numberInstance: "+obj1.numberInstance);
 
 
         C06_StaticVariables obj2 = new C06_StaticVariables();
         obj2.numberStatic++;
         obj2.numberInstance++;
 
-        System.out.println("obj2 numberStatic: "+obj2.numberStatic+ " obj1 numberInstance: "+obj2.numberInstance);
 
 
         C06_StaticVariables obj3 = new C06_StaticVariables();
@@ -27,11 +25,12 @@ public class C06_StaticVariables {
 
 
         System.out.println("obj1 numberStatic: "+obj1.numberStatic+ " obj1 numberInstance: "+obj1.numberInstance);
+        //obj1 numberStatic: 23 obj1 numberInstance: 41
 
         System.out.println("obj2 numberStatic: "+obj2.numberStatic+ " obj1 numberInstance: "+obj2.numberInstance);
+        //obj2 numberStatic: 23 obj1 numberInstance: 41
 
         System.out.println("obj3 numberStatic: "+obj3.numberStatic+ " obj1 numberInstance: "+obj3.numberInstance);
-
-
+        //obj3 numberStatic: 23 obj1 numberInstance: 41
     }
 }
